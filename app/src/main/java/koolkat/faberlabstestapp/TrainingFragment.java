ce5583a9f65e571b17256505958042bd8acbce87
narayanasuri <Narayanasuri08@gmai.com>
2017-05-12 06:23:38 -0700

    Implemented all the tabs in the performance fragment.

--- app/src/main/java/koolkat/faberlabstestapp/TrainingFragment.java
+++ app/src/main/java/koolkat/faberlabstestapp/TrainingFragment.java
@@ -14,7 +14,6 @@ import android.widget.Toast;
 
 public class TrainingFragment extends Fragment implements View.OnClickListener {
 
-    Button endsessionbtn;
 
     public static TrainingFragment newInstance() {
         return new TrainingFragment();
@@ -23,14 +22,10 @@ public class TrainingFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.training_fragment, container, false);
-        endsessionbtn = (Button) view.findViewById(R.id.endsessionbtn);
-        endsessionbtn.setOnClickListener(this);
         return view;
     }
 
     @Override
     public void onClick(View v) {
-        if(v==endsessionbtn)
-            Toast.makeText(getContext(), "Session Ended!", Toast.LENGTH_SHORT).show();
     }
 }
